58ab4e59bcd0daf29e5a3fe9344276803bd73b7f
NoNiMad <nonimad@free.fr>
2018-11-08 14:59:58 +0100

    Biomes have coordinates now

---
+++ Assets/Scripts/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestController : MonoBehaviour {
+    public BiomeManager Manager;
+    public Transform Camera;
+
+	void Update () {
+		if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.position, Camera.TransformDirection(Vector3.forward), out hit))
+            {
+                Debug.DrawRay(Camera.position, Camera.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                BiomeController biome = hit.transform.parent.GetComponent<BiomeController>();
+                Debug.Log(hit.transform.name);
+                biome.RemoveBlock(hit.transform.name);
+            }
+        }
+	}
+}

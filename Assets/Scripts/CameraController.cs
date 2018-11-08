b3098a2e0cffd21209e4d75ca140ea5827579639
Axel Soll <axel.soll@telecom-paristech.fr>
2018-11-08 19:38:07 +0100

    function rotateCamera ready for action

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,6 +17,10 @@ public class CameraController : MonoBehaviour {
 	private Vector3 camTarget;
 	private Vector3Int currentBiomePos;
 	private float delta;
+	//testing
+	private bool is_done_rotating = false;
+
+
 	private Vector3 rotateOffset90(Vector3 v, int direction) {
 		Quaternion q;
 		if(direction >= 0) {
@@ -38,19 +42,32 @@ public class CameraController : MonoBehaviour {
 		nextCamOffset = rotateOffset90(prevCamOffset, 1);
 		delta = 0;
 		transform.position = currentBiomePos + biomeCenterOffset + curCamOffset;
-		// StartCoroutine(waiter());
 	}
 
-	// IEnumerator waiter()
-	// {
-	// }
-	// Update is called once per frame
-	void Update () {
+	// direction should either be 1 or -1 (but can take any positive or negative value with the same result)
+	// Example :
+	// 	if(!is_done_rotating)
+	// 		is_done_rotating = rotateCamera(1, 1.2f);
+	public bool rotateCamera(int direction = 1, float speed = 1f) {
+		nextCamOffset = rotateOffset90(prevCamOffset, direction);
 		camTarget = currentBiomePos + biomeCenterOffset;
-		delta = delta + Time.deltaTime;
+		delta = Mathf.Min(1, delta + speed*Time.deltaTime);
 		curCamOffset = Vector3.Slerp(prevCamOffset, nextCamOffset,delta);
 		transform.position = currentBiomePos + biomeCenterOffset + curCamOffset;
 		transform.LookAt(camTarget, Vector3.up);
+		if (curCamOffset == nextCamOffset){
+			prevCamOffset = nextCamOffset;
+			delta = 0;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	// Update is called once per frame
+	// Example
+	void Update () {
+		if(!is_done_rotating)
+			is_done_rotating = rotateCamera(1, 1.2f);
 		Debug.Log(transform.position);
 	}
 }

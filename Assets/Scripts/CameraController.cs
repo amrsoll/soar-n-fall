4eba03b33e6a811c8a24a90dd80916fde3338340
Axel Soll <axel.soll@telecom-paristech.fr>
2018-11-09 18:02:26 +0100

    these camControls are zzmoooth

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
+
 public class CameraController : MonoBehaviour {
+
+	public const byte LEFT = 1;
+	public const byte RIGHT = 2;
+	private const float DIST_TO_BIOME = 1f;
+	private const float DIST_TO_PLAYER = 0.5f;
 
 	public GameObject player;       //Public variable to store a reference to the player game object
 	public BiomeManager biomeManager;
@@ -15,13 +22,22 @@ public class CameraController : MonoBehaviour {
 	private Vector3 curCamOffset;         //Private variable to store the offset distance between the player and camera
 	private Vector3 nextCamOffset;         //Private variable to store the offset distance between the player and camera
 	private Vector3 camTarget;
+	private float camDistance;
 	private Vector3Int currentBiomePos;
-	private float delta;
+	// private float delta;
 	//testing
-	private bool is_done_rotating = true;
+	private bool isDoneRotating = true;
+	private byte rotationDirection = 0;
+	private bool followThePlayer = false;
 
+	// returns true if the camera is already centered on the position v
+	private bool Follow(Vector3 v, float distance = 1f, float strength = 2f) {
+		//TODO emulate distance with the size of the viewfield
+		transform.position = Vector3.Slerp(transform.position, v+curCamOffset, strength*Time.deltaTime);
+		return transform.position == v+distance*curCamOffset;
+	}
 
-	private Vector3 rotateOffset90(Vector3 v, int direction) {
+	private Vector3 RotateBy90Up(Vector3 v, int direction) {
 		Quaternion q;
 		if(direction >= 0) {
 			q = Quaternion.AngleAxis( 90, Vector3.up);
@@ -29,6 +45,30 @@ public class CameraController : MonoBehaviour {
 			q = Quaternion.AngleAxis(-90, Vector3.up);
 		}
 		return q * v;
+	}
+
+	// direction should either be LEFT or RIGHT (but can take any positive or negative value with the same result)
+	// Example :
+	// 	if(!isDoneRotating)
+	// 		isDoneRotating = rotateCamera(RIGHT, 1.2f);
+	public void rotateCamera(byte direction = 1) {
+		int dir = 0;
+		if(direction == LEFT) dir = 1;
+		if(direction == RIGHT) dir = -1;
+		curCamOffset = RotateBy90Up(curCamOffset, dir);
+		Debug.Log(curCamOffset);
+	}
+
+	public void SwitchView() {
+		followThePlayer = !followThePlayer;
+		if(followThePlayer) {
+			camTarget = player.transform.position;
+			camDistance = DIST_TO_PLAYER;
+		} else {
+			camTarget = currentBiomePos + biomeCenterOffset;
+			camDistance = DIST_TO_BIOME;
+		}
+
 	}
 
 	// Use this for initialization
@@ -39,37 +79,26 @@ public class CameraController : MonoBehaviour {
 																currentBiomePos.z);
 		prevCamOffset = new Vector3(5f,5f,5f);
 		curCamOffset = prevCamOffset;
-		nextCamOffset = rotateOffset90(prevCamOffset, 1);
-		delta = 0;
-		transform.position = currentBiomePos + biomeCenterOffset + curCamOffset;
-		transform.LookAt(camTarget, Vector3.up);
-	}
-
-	// direction should either be 1 or -1 (but can take any positive or negative value with the same result)
-	// Example :
-	// 	if(!is_done_rotating)
-	// 		is_done_rotating = rotateCamera(1, 1.2f);
-	public bool rotateCamera(int direction = 1, float speed = 1f) {
-		nextCamOffset = rotateOffset90(prevCamOffset, direction);
+		nextCamOffset = RotateBy90Up(prevCamOffset, 1);
 		camTarget = currentBiomePos + biomeCenterOffset;
-		delta = Mathf.Min(1, delta + speed*Time.deltaTime);
-		curCamOffset = Vector3.Slerp(prevCamOffset, nextCamOffset,delta);
-		transform.position = currentBiomePos + biomeCenterOffset + curCamOffset;
+		camDistance = DIST_TO_BIOME;
+		transform.position = camTarget + curCamOffset;
 		transform.LookAt(camTarget, Vector3.up);
-		if (curCamOffset == nextCamOffset){
-			prevCamOffset = nextCamOffset;
-			delta = 0;
-			return true;
-		} else {
-			return false;
-		}
 	}
 
 	void Update() {
-    	if (Input.GetMouseButtonUp(1)){
-            is_done_rotating = false;
+    	if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.JoystickButton5)){
+			rotateCamera(RIGHT);
         }
-        if(!is_done_rotating)
-    		is_done_rotating = rotateCamera(1, 1.2f);
+		if(Input.GetKeyDown(KeyCode.JoystickButton4)) {
+			rotateCamera(LEFT);
+		}
+		if(Input.GetKeyDown(KeyCode.V)) {
+			SwitchView();
+		}
+		if(followThePlayer)
+			camTarget = player.transform.position; //needs to be refreshed
+		Follow(camTarget, camDistance);
+		transform.LookAt(camTarget, Vector3.up);
 	}
 }

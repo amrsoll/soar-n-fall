d84e4e03a54f654685e7c4e611d493860c3ea403
Dui Ardal <duiardal@gmail.com>
2018-12-12 14:11:57 +0100

    Added animations to the character and Respawn for pauseMenu

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -6,19 +6,22 @@ public class Respawn : MonoBehaviour {
 
 	public Vector3 startPosition;
 	public Quaternion startRotation;
+    private GameObject player;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
-		startPosition = this.transform.position;
-		startRotation = this.transform.rotation;
+        player = GameObject.FindWithTag("Player");
+
+		startPosition = player.transform.position;
+		startRotation = player.transform.rotation;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//change to: look if you've changed biome
-		if (this.transform.position.y < -50.0) {
+		if (player.transform.position.y < -50.0) {
 			respawnPlayer();
 			return;
 		}
@@ -26,7 +29,12 @@ public class Respawn : MonoBehaviour {
 	}
 
 	public void respawnPlayer() {
-		this.transform.rotation = startRotation;
-		this.transform.position = startPosition;
-	}
+        Debug.Log("WTF?!");
+        player.transform.rotation = startRotation;
+        player.transform.position = startPosition;
+
+        if (PauseMenu.GameIsPaused) {
+            GetComponent<PauseMenu>().Resume();
+        }
+    }
 }

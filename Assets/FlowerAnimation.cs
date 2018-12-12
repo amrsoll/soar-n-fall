ebf676f573d74ea0f63e62d6ebbf4efa3f2383d8
Petra <petraol@kth.se>
2018-12-12 21:36:50 +0100

    finished flower animation

---
+++ Assets/FlowerAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerAnimation : MonoBehaviour {
+
+	Animator anim;
+	Collider m_Collider;
+ 
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetTrigger("Active");
+            m_Collider = GetComponent<Collider>();
+            m_Collider.enabled = true;
+        }
+    }
+}

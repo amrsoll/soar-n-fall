8ebf40e979c194ebda53b0180269d81b256adf87
Petra Olsson <petraol@kth.se>
2019-09-18 14:36:36 +0200

    fixed waterpot interaction

--- Assets/Scripts/Items/rsc/WaterPot.cs
+++ Assets/Scripts/Items/rsc/WaterPot.cs
@@ -2,30 +2,29 @@ using UnityEngine;
 // TODO it's actually watering pot
 public class WaterPot : ItemController
 {
-    private bool hasWater;
+    Animator anim;
+    Collider m_Collider;
 
-    public void Start()
+    /*public void Start()
     {
         hasWater = false;
         hasWater = true;
-    }
-    
+    }*/
+
     public override bool InteractWith(ItemController item)
     {
-        if (item.type == Item.Flower)//&& hasWater)
+        if (item.type == Item.Flower)
         {
+            anim = item.GetComponent<Animator>();
             GameObject.Find("SoundManager").GetComponent<NarrationTrigger>().PlayClip("on-flowers-watered");
-            item.GetComponent<Animator>().SetTrigger("Active");
-            item.GetComponent<Collider>().enabled = true;
-            // water the flower
-            // delete the water in the waterpot
-            hasWater = !hasWater;
-            return true;
+            anim.SetTrigger("Grow");
+            m_Collider = item.GetComponent<Collider>();
+            m_Collider.enabled = true;
         }
         return false;
     }
     
-    public override bool InteractWith(BlockController block)
+    /*public override bool InteractWith(BlockController block)
     {
         if (block.type == BlockType.Water)
         {
@@ -35,7 +34,7 @@ public class WaterPot : ItemController
             return true;
         }
         return false;
-    }
+    }*/
     
     
 }
